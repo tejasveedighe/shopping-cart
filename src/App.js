110f701aa1fc8c24d4665ef89403788abc538e97
tejasvee-appsdeployer <tejasvee@appsdeployer.com>
2023-03-12 12:31:51 +0530

    Basic Input form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [input, setInput] = useState();
+
+  function handleChange(event) {
+    event.preventDefault();
+    setInput(event.currentTarget.value)
+  }
+  
+    return(<div className="App">
+      <form>
+      <input type="number" value={input} placeholder='Enter Cart Items' onChange={handleChange}/>
+      <button >Checkout</button>
+      </form>
     </div>
   );
 }
