8026bf778e8cd39e38ed7d384cef8ab88219c98a
Tejasvee Dighe <tejasveedighe@gmail.com>
2023-03-12 13:52:51 +0530

    Logic To add the item to the least sum cart lane

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-	const [input, setInput] = useState();
-	const [lanes, setLanes] = useState([[], [], [], [], []]);
+const initialInput = "";
 
-	function handleChange(event) {
-		event.preventDefault();
-		setInput(event.currentTarget.value);
-	}
+function App() {
+	const [input, setInput] = useState(initialInput);
+	const [lanes, setLanes] = useState([[], [], [], [], []]);
 
 	function handleSubmit(event) {
 		event.preventDefault();
+
+		// select the lane with the least sum
+		let selectedIndex,
+			currSum = 0,
+			prevSum = Number.MAX_VALUE;
+		lanes.forEach((lane, index) => {
+			currSum = lane.reduce((item, val) => {
+				return item + val;
+			}, 0);
+			if (currSum < prevSum) {
+				selectedIndex = index;
+				prevSum = currSum;
+				currSum = 0;
+			}
+		});
+
+		// push the value to that lane
+		let newLanes = lanes;
+		newLanes[selectedIndex].push(input);
+
+		setInput(initialInput);
+		setLanes(newLanes);
 	}
 
 	return (
 		<div className="App">
 			<form onSubmit={handleSubmit}>
 				<input
-					type="number"
 					value={input}
+					type="number"
 					placeholder="Enter Cart Items"
-					onChange={handleChange}
+					onChange={(e) => setInput(e.target.valueAsNumber)}
 				/>
-				<button>Checkout</button>
+				<button type="submit">Checkout</button>
 			</form>
 
 			<div className="lanes">
@@ -31,6 +50,13 @@ function App() {
 					return (
 						<li className="lane" key={index}>
 							X
+							{lane.map((item, index) => {
+								return (
+									<li className="lane-item" key={index}>
+										{item}{" "}
+									</li>
+								);
+							})}
 						</li>
 					);
 				})}
