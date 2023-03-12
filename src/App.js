3cc50297e6ba9267c7b32eeea39115d9a5196a96
Tejasvee Dighe <tejasveedighe@gmail.com>
2023-03-12 13:21:04 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [input, setInput] = useState();
+	const [input, setInput] = useState();
+	const [lanes, setLanes] = useState([[], [], [], [], []]);
 
-  function handleChange(event) {
-    event.preventDefault();
-    setInput(event.currentTarget.value)
-  }
-  
-    return(<div className="App">
-      <form>
-      <input type="number" value={input} placeholder='Enter Cart Items' onChange={handleChange}/>
-      <button >Checkout</button>
-      </form>
-    </div>
-  );
+	function handleChange(event) {
+		event.preventDefault();
+		setInput(event.currentTarget.value);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	return (
+		<div className="App">
+			<form onSubmit={handleSubmit}>
+				<input
+					type="number"
+					value={input}
+					placeholder="Enter Cart Items"
+					onChange={handleChange}
+				/>
+				<button>Checkout</button>
+			</form>
+
+			<div className="lanes">
+				{lanes.map((lane, index) => {
+					return (
+						<li className="lane" key={index}>
+							X
+						</li>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
